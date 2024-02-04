1631fde9938f3d0aa4614c323335b25cbe195597
JuliaJPereira <juliapereira.odc@gmail.com>
2024-02-04 16:06:40 -0300

    Aula 3.3. React-countries – Implementação do input de filtro de países

--- src/pages/ReactCountriesPage.jsx
+++ src/pages/ReactCountriesPage.jsx
@@ -1,10 +1,28 @@
+import { useState } from 'react';
 import Header from '../components/Header';
 import Main from '../components/Main';
+import { allCountries } from '../data/countries';
+import TextInput from './../components/TextInput';
+
 export default function ReactCountriesPage() {
+  const [ContryFilter, setCountryFilter] = useState('Argentina');
+
+  function handleCountryFilterChange(newCountryFilter) {
+    setCountryFilter(newCountryFilter);
+  }
+
   return (
     <div>
       <Header>react-countries</Header>
-      <Main>Conteúdo</Main>
+      <Main>
+        <TextInput
+          id="inputCountryFilter"
+          labelDescription="Informe o nome do país (pelo menos 3 caracteres):"
+          inputValue={ContryFilter}
+          autoFocus
+          onInputChange={handleCountryFilterChange}
+        />
+      </Main>
     </div>
   );
 }
