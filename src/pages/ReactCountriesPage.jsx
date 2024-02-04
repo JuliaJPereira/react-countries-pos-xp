2f29a3f4a36128d36402f1461eac04a19e508484
JuliaJPereira <juliapereira.odc@gmail.com>
2024-02-04 15:55:58 -0300

    Aula 3.2. React-countries – Organização do projeto

---
+++ src/pages/ReactCountriesPage.jsx
@@ -0,0 +1,10 @@
+import Header from '../components/Header';
+import Main from '../components/Main';
+export default function ReactCountriesPage() {
+  return (
+    <div>
+      <Header>react-countries</Header>
+      <Main>Conteúdo</Main>
+    </div>
+  );
+}
